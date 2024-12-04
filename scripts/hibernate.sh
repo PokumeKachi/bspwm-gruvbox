54c07a90c6f46ef17dcbd69b1f2841cda6be8ce0
PokumeKachi <oneoneninenineyouaremine@gmail.com>
2024-12-04 21:54:55 +0700

    added stuff

--- scripts/hibernate.sh
+++ scripts/hibernate.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# Get the total used RAM (in KB)
 used_ram=$(free | awk '/^Mem:/ { print $3 }')
 
-# Get the total swap size (in KB)
 swap_size=$(free | awk '/^Swap:/ { print $2 }')
 state=$(upower -i $(upower -e | grep 'BAT') | grep -i 'state' | awk '{print $2}')
 
@@ -11,16 +9,23 @@ if [ "$used_ram" -gt "$swap_size" ]; then
 	echo "Can't hibernate, too much ram used"
 else
 #    if [ "$state" = "discharging" ]; then
-  PASSWORD=$(zenity --password --timeout=5 --title="Authentication Required")
+  pkill zenity
+  PASSWORD=$(zenity --password --timeout=15 --title="Authentication Required")
 
   if [ -z "$PASSWORD" ]; then
     echo "No password provided"
     exit
   fi
 
-	echo "Hibernatin' time baby"
-  echo $PASSWORD | sudo -S rmmod xmm7360
-  echo $PASSWORD | sudo -S systemctl hibernate
+  echo "$PASSWORD" | su -c "echo 'Hibernatin time baby'" -s /bin/bash "root"
+
+  if [ $? -ne 0 ]; then
+    echo "Incorrect password"
+  else
+    echo $PASSWORD | sudo -S rmmod xmm7360
+    echo $PASSWORD | sudo -S systemctl hibernate
+  fi
+
 #    else
 #	systemctl suspend
 #    fi
