c118030cf4d0ffbf809dd2d3966bfaa0ae52eebb
PokumeKachi <oneoneninenineyouaremine@gmail.com>
2025-01-21 17:59:25 +0700

    prob final commit

--- scripts/hibernate.sh
+++ scripts/hibernate.sh
@@ -9,22 +9,25 @@ if [ "$used_ram" -gt "$swap_size" ]; then
 	echo "Can't hibernate, too much ram used"
 else
 #    if [ "$state" = "discharging" ]; then
-  pkill zenity
-  PASSWORD=$(zenity --password --timeout=15 --title="Authentication Required")
-
-  if [ -z "$PASSWORD" ]; then
-    echo "No password provided"
-    exit
-  fi
-
-  echo "$PASSWORD" | su -c "echo 'Hibernatin time baby'" -s /bin/bash "root"
-
-  if [ $? -ne 0 ]; then
-    echo "Incorrect password"
-  else
-    echo $PASSWORD | sudo -S rmmod xmm7360
-    echo $PASSWORD | sudo -S systemctl hibernate
-  fi
+  # pkill zenity
+  # PASSWORD=$(zenity --password --timeout=15 --title="Authentication Required")
+  #
+  # if [ -z "$PASSWORD" ]; then
+  #   echo "No password provided"
+  #   exit
+  # fi
+  #
+  # echo "$PASSWORD" | su -c "echo 'Hibernatin time baby'" -s /bin/bash "root"
+  #
+  # if [ $? -ne 0 ]; then
+  #   echo "Incorrect password"
+  # else
+  #   echo $PASSWORD | sudo -S rmmod xmm7360
+  #   echo $PASSWORD | sudo -S systemctl hibernate
+  # fi
+  
+  systemctl hibernate
+  echo 'Hibernatin time baby'
 
 #    else
 #	systemctl suspend
